dfa26e47f8820618ef9f6b0f88eace5b2cc4f73e
surelis segarra <sdarianne34@gmail.com>
2020-05-10 15:28:14 -0700

    created history item component

--- src/components/HIstory/HistoryItem.jsx
+++ src/components/HIstory/HistoryItem.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const HistoryItem = () => ();
+const HistoryItem = ({ url, method }) => (
+  <section>
+    <p>{url}</p>
+    <p>{method}</p>
+  </section>
+);
 
-History.propTypes = {};
+HistoryItem.propTypes = {
+  url: PropTypes.string.isRequired,
+  method: PropTypes.string.isRequired
+};
 
 export default HistoryItem;
