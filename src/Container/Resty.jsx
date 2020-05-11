a9c57e1c55724f429a525a419807789594277b82
surelis segarra <sdarianne34@gmail.com>
2020-05-10 22:42:34 -0700

    local storage works

--- src/Container/Resty.jsx
+++ src/Container/Resty.jsx
@@ -1,22 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Request from '../components/Request/Request';
 import { makeRequest } from '../services/makeRequest';
 import Response from '../components/Response/Response';
+import HistoryList from '../components/HIstory/HistoryList';
+
+const getHistory = () => localStorage.getItem('history') ? JSON.parse(localStorage.getItem('history')) : [];
 
 const Resty = () => {
   const [url, setUrl] = useState('');
   const [method, setMethod] = useState('GET');
   const [body, setBody] = useState('');
   const [response, setResponse] = useState({});
+  const [history, setHistory] = useState(getHistory());
   
+  //whenecer history changes, update history of local storage
+  useEffect(() => {
+    localStorage.setItem('history', JSON.stringify(history));
+
+  }, [history]);
+
   // const handleUrlChange = ({ target }) => {
   //   setUrl(target.value);
   // };
-
   // const handleMethodChange = ({ target }) => {
   //   setMethod(target.value);
   // };
-
   // const handleBodyChange = ({ target }) => {
   //   setBody(target.value);
   // };
@@ -30,11 +38,17 @@ const Resty = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     makeRequest(url, method, body)
-      .then(json => setResponse(json));
+      .then(json => {
+        setResponse(json);
+        //we take out prev history, we create a new arr, we put the new obj with our curr url and method. then we spread all of our old historu into the array 
+        setHistory(prevHistory  => [{ url, method }, ...prevHistory]);
+        
+      });
   };
 
   return (
     <>
+      <HistoryList history={history}/>
       <Request 
         url={url} 
         method={method} 
