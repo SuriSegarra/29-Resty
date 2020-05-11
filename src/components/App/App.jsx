0e04654bf68f906ba620412fdbe05ba5efd8730f
surelis segarra <sdarianne34@gmail.com>
2020-05-10 22:49:27 -0700

    HistoryItem test passed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 import Resty from '../../Container/Resty';
+import Header from '../Header/Header';
 
 export default function App() {
   return (
-    <Resty/>
+    <>
+      <Header/>
+      <Resty/>
+    </>
   );
 }
